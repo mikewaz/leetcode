17acb251a078996404e2d4cb1c56cef43afae467
Mark Saushkin <mark.saushkin@outlook.com>
2026-07-16 18:39:37 +0400

    Reorganize project: refactor Solution142 implementation and tests into new folder structure

--- Google_tests/Sol000sTests/Sol100s/Sol140s/Sol142/Sol142Test.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Марк
-// Ref https://leetcode.com/problems/linked-list-cycle-ii/
-//
-
-#include <gtest/gtest.h>
-TEST(Sol142, detectCycle) {
-
-}
---
+++ solutions/01_medium/S0142.cpp
@@ -0,0 +1,33 @@
+﻿//
+// Created by marks on 16.07.2026.
+// https://leetcode.com/problems/linked-list-cycle-ii
+//
+
+#include "solutions/ListNode.h"
+
+namespace medium {
+    class S142 {
+    public:
+        static ListNode *detectCycle(ListNode *head) {
+            ListNode* slow = head;
+            ListNode* fast = head;
+
+            while (fast != nullptr && fast->next != nullptr) {
+                slow = slow->next;
+                fast = fast->next->next;
+
+                if (slow == fast) {
+                    slow = head;
+
+                    while (slow != fast) {
+                        slow = slow->next;
+                        fast = fast->next;
+                    }
+
+                    return slow;
+                }
+            }
+
+            return nullptr;
+    };
+}
