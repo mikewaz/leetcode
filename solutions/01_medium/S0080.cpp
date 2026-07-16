1ffe398d9c554c30757be8f0790143024f9dba0d
Mark Saushkin <mark.saushkin@outlook.com>
2026-07-16 18:34:17 +0400

    Reorganize project: refactor Solution80 implementation and tests into new folder structure

---
+++ solutions/01_medium/S0080.cpp
@@ -0,0 +1,28 @@
+﻿//
+// Created by marks on 16.07.2026.
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+//
+
+#include <vector>
+
+namespace medium {
+    class S80 {
+    public:
+        static int removeDuplicates(std::vector<int> &nums) {
+            auto result = 2;
+
+            if (nums.size() <= 2)
+                return nums.size();
+
+            for (int i = 2; i < nums.size(); i++) {
+                if (nums[i] != nums[result - 2]) {
+
+                    nums[result] = nums[i];
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    };
+}
---
+++ tests/01_medium/S0080_tests.cpp
@@ -0,0 +1,15 @@
+﻿//
+// Created by marks on 16.07.2026.
+// https://leetcode.com/problems/linked-list-cycle-ii/
+//
+
+#include <gtest/gtest.h>
+#include <vector>
+#include "solutions/01_medium/S0080.cpp"
+
+TEST(Sol80, removeDuplicates) {
+    std::vector<int> nums = {1, 1, 1, 2, 2, 3, 3, 4, 4, 4, 4};
+    auto actual = medium::S80::removeDuplicates(nums);
+
+    EXPECT_EQ(actual, 8);
+}
