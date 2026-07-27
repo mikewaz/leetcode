749b71285deea148def95031184f44a0c6d17c50
Mark Saushkin <mark.saushkin@outlook.com>
2026-07-27 14:20:30 +0400

    Problem Solution "1464. Maximum Product of Two Elements in an Array"

---
+++ solutions/00_easy/S1464.cpp
@@ -0,0 +1,27 @@
+﻿//
+// Created by marks on 27.07.2026.
+// https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/
+//
+
+#include <vector>
+
+namespace easy {
+    class S1464 {
+    public:
+        int maxProduct(std::vector<int> &nums) {
+            int l = 0, r = 0;
+
+            for (const auto &item: nums) {
+                if (item > l) {
+                    r = l;
+                    l = item;
+                }
+                else if (item > r) {
+                    r = item;
+                }
+            }
+
+            return (l - 1) * (r - 1);
+        }
+    };
+}
---
+++ tests/00_easy/S1464Test.cpp
@@ -0,0 +1,13 @@
+﻿//
+// Created by marks on 27.07.2026.
+//
+
+#include <gtest/gtest.h>
+#include "solutions/00_easy/S1464.cpp"
+
+TEST(S1464, maxProduct) {
+    std::vector<int> nums {3,4,5,2};
+    easy::S1464 s;
+    auto actual = s.maxProduct(nums);
+    EXPECT_EQ(actual, 12);
+}
